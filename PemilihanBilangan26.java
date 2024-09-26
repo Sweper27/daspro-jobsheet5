e5c5998400d0b6c64882c1e3a9d34274924c3d05
Sweper27 <rifqiariessaputra@gmail.com>
2024-09-26 08:16:56 +0700

    menganti operasi if else menjadi operasi ternary

--- PemilihanBilangan26.java
+++ PemilihanBilangan26.java
@@ -7,14 +7,9 @@ public class PemilihanBilangan26 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-         {
-         System.out.println("angka" + angka + "termasuk bilangan genap");   
-        }
-        else
-        {
-            System.out.println("angka " + angka + " termasuk bilangan ganjil");
-        }
+        String Hasil = (angka % 2==0)? "genap" : "ganjil";
+
+        System.out.println(angka + " adalah Bilangan " + Hasil);
     }
     
 }
